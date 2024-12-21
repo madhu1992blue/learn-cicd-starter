52cc5a4eec45ade72882593ca4c137d40abab87e
Madhusudan N <madhu1992blue@outlook.com>
2024-12-21 13:56:01 +0530

    chore: add formatting test to CI

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,22 +1,23 @@
 package auth
+
 import (
-	"testing"
 	"net/http"
+	"testing"
 )
 
 func TestNoAuthHeaderErrors(t *testing.T) {
-	noAuthHeader := &http.Header {}
+	noAuthHeader := &http.Header{}
 	_, err := GetAPIKey(*noAuthHeader)
-	if err !=  ErrNoAuthHeaderIncluded{
+	if err != ErrNoAuthHeaderIncluded {
 		t.Fatalf("Wanted error: %v but didn't receive one", ErrNoAuthHeaderIncluded)
 	}
 }
 
 func TestValidApiKeyHeader(t *testing.T) {
-        validHeader := &http.Header {}
+	validHeader := &http.Header{}
 	validHeader.Set("Authorization", "ApiKey abc")
-        key, err := GetAPIKey(*validHeader)
-        if err !=  nil || key != "abc" {
+	key, err := GetAPIKey(*validHeader)
+	if err != nil || key != "abc" {
 		t.Fatalf("Wanted key: %s, Got key: %s", "abc", key)
-        }
+	}
 }
